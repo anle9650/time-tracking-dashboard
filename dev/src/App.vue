2c2b2076f926e972cf5a3e946cf772eafa8969ce
anle9650 <andy.le@ucdenver.edu>
2022-03-05 18:55:22 -0700

    Update App.vue

--- dev/src/App.vue
+++ dev/src/App.vue
@@ -1,74 +1,103 @@
 <template>
   <section class="container my-auto">
-      <div class="row">
-        <!-- Left Column -->
-        <div class="col-lg-3 mb-4">
-          <ProfileCard @select-timeframe="(timeframe) => this.timeframe = timeframe" />
+    <div class="row">
+      <!-- Left Column -->
+      <div class="col-lg-3 mb-4">
+        <ProfileCard
+          @select-timeframe="(timeframe) => (this.timeframe = timeframe)"
+        />
+      </div>
+
+      <!-- Right Column -->
+      <div class="col-lg">
+        <!-- Row 1 -->
+        <div class="row">
+          <div class="col-lg mb-4">
+            <ActivityCard
+              activity="work"
+              :activityData="activities[0]"
+              :timeframe="timeframe"
+            />
+          </div>
+          <div class="col-lg mb-4">
+            <ActivityCard
+              activity="play"
+              :activityData="activities[1]"
+              :timeframe="timeframe"
+            />
+          </div>
+          <div class="col-lg mb-4">
+            <ActivityCard
+              activity="study"
+              :activityData="activities[2]"
+              :timeframe="timeframe"
+            />
+          </div>
         </div>
 
-        <!-- Right Column -->
-        <div class="col-lg">
-          <!-- Row 1 -->
-          <div class="row">
-            <div class="col-lg mb-4">
-              <ActivityCard activity="work" :activityData="activities[0]" :timeframe="timeframe" />
-            </div>
-            <div class="col-lg mb-4">
-              <ActivityCard activity="play" :activityData="activities[1]" :timeframe="timeframe" />
-            </div>
-            <div class="col-lg mb-4">
-              <ActivityCard activity="study" :activityData="activities[2]" :timeframe="timeframe" />
-            </div>
+        <!-- Row 2 -->
+        <div class="row">
+          <div class="col-lg mb-4">
+            <ActivityCard
+              activity="exercise"
+              :activityData="activities[3]"
+              :timeframe="timeframe"
+            />
           </div>
-
-          <!-- Row 2 -->
-          <div class="row">
-            <div class="col-lg mb-4">
-              <ActivityCard activity="exercise" :activityData="activities[3]" :timeframe="timeframe" />
-            </div>
-            <div class="col-lg mb-4">
-              <ActivityCard activity="social" :activityData="activities[4]" :timeframe="timeframe" />
-            </div>
-            <div class="col-lg mb-4">
-              <ActivityCard activity="self-care" :activityData="activities[5]" :timeframe="timeframe" />
-            </div>
+          <div class="col-lg mb-4">
+            <ActivityCard
+              activity="social"
+              :activityData="activities[4]"
+              :timeframe="timeframe"
+            />
+          </div>
+          <div class="col-lg mb-4">
+            <ActivityCard
+              activity="self-care"
+              :activityData="activities[5]"
+              :timeframe="timeframe"
+            />
           </div>
         </div>
       </div>
-    </section>
-
-    <div class="attribution">
-      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="#">Andy Le</a>.
     </div>
+  </section>
+
+  <div class="attribution">
+    Challenge by
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+      >Frontend Mentor</a
+    >. Coded by <a href="#">Andy Le</a>.
+  </div>
 </template>
 
 <script>
-import ProfileCard from './components/ProfileCard.vue'
-import ActivityCard from './components/ActivityCard.vue'
-import activityData from './assets/data.json';
+import ProfileCard from "./components/ProfileCard.vue";
+import ActivityCard from "./components/ActivityCard.vue";
+import activityData from "./assets/data.json";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     ProfileCard,
-    ActivityCard
+    ActivityCard,
   },
   data() {
     return {
       activities: activityData,
-      timeframe: 'daily'
-    }
-  }
-}
+      timeframe: "daily",
+    };
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap");
 
 html,
 body {
   height: 100%;
-  font-family: 'Rubik', sans-serif;
+  font-family: "Rubik", sans-serif;
   background-color: hsl(226, 43%, 10%);
 }
 
